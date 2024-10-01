7198b2c174c1dd19117eddde6951818b4a815aac
archana-singh-1 <archana21@navgurukul.org>
2024-10-01 10:24:38 +0530

    completed signup_page

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from 'mongoose';
 import router from './routes/hiring-route.js'
+import signup from './routes/signupRoute.js'
+import login from "./routes/signupRoute.js"
 
 
 const app=express();
@@ -17,12 +19,6 @@ mongoose.connect(url)
     console.log(err)
 })
 
-
-
-
-
-const mongoPassword = process.env.MONGODB_PASSWORD
-const url = `mongodb+srv://aayushisharma1:${mongoPassword}@cluster0.jfztl.mongodb.net/Job_Protal`
 mongoose.connect(url)
 .then(()=>{
     console.log("Connect Done")
@@ -33,9 +29,9 @@ mongoose.connect(url)
 
 
 
-app.use(express.json());
-
 app.use('/hiring',router)
+app.use("/user",signup)
+app.use("/login",login)
 
 app.listen(PORT,function(){
     console.log("Server is running on port 4000")
