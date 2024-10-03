8afd0a56b6aec3569bbacd23000edaac07cfcc3e
Aayushi Sharma <aayushisharma20@navgurukul.org>
2024-10-03 15:40:51 +0530

    Added Get API

--- controllers/hiring.js
+++ controllers/hiring.js
@@ -18,4 +18,22 @@ const post_data = async (req, res) => {
     }
 }
 
-export default post_data;
+
+const get_data = async (req,res)=>{
+    try{
+        const data = await Company.find({})
+        res.send({
+            message:"done",
+            details:data
+        })
+        
+    }
+    catch{
+        res.send({
+            message:"err",
+            err:err
+        })
+
+    }
+}
+export  {post_data,get_data};
