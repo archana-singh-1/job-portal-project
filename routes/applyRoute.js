ad6b3dce689ade3a68f2552c6e4198dfde8b2d0f
Aayushi Sharma <aayushisharma20@navgurukul.org>
2024-10-18 19:09:23 +0530

    Add Apply Schema

--- routes/applyRoute.js
+++ routes/applyRoute.js
@@ -1,11 +1,10 @@
 
 import express from "express"; 
-import { applyForJob } from '../controllers/Apply.js';
-import upload from "../middleware/multer.js";
+import { upload,applyForJob } from '../controllers/Apply.js';
+
 
 const router = express.Router();
 
-router.post("/apply-job/:jobId", upload.single('resume'), applyForJob);
 
 
 export default router;
